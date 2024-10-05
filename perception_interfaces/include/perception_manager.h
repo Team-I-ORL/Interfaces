77b1f8990619c511efb7230eab42d61e95dde735
Jinkai <jinkaiq@andrew.cmu.edu>
2024-10-05 14:34:33 -0400

    offloading unage subscriber to perception manager

--- perception_interfaces/include/perception_manager.h
+++ perception_interfaces/include/perception_manager.h
@@ -15,6 +15,11 @@ class PerceptionManager : public rclcpp::Node
         rclcpp::Client<perception_interfaces::srv::Segmask>::SharedPtr _segmask_client;
         rclcpp::Client<perception_interfaces::srv::Sucpose>::SharedPtr _sucpose_client;
         rclcpp::CallbackGroup::SharedPtr _client_callback_group;
+
+        rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr _color_image_sub;
+        rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr _depth_image_sub;
+        rclcpp::Subscription<sensor_msgs::msg::CameraInfo>::SharedPtr _camera_info_sub;
+        rclcpp::CallbackGroup::SharedPtr _subscription_callback_group;
         
         tf2_ros::Buffer tfBuffer;
         tf2_ros::TransformListener tf_listener;
@@ -37,7 +42,22 @@ class PerceptionManager : public rclcpp::Node
             _sucpose_received = true;
         }
 
+        void _color_image_callback(const sensor_msgs::msg::Image::SharedPtr msg){
+            _color_image = *msg;
+        }
+
+        void _depth_image_callback(const sensor_msgs::msg::Image::SharedPtr msg){
+            _depth_image = *msg;
+        }
+
+        void _camera_info_callback(const sensor_msgs::msg::CameraInfo::SharedPtr msg){
+            _camera_info = *msg;
+        }
+
         sensor_msgs::msg::Image _segmask;
+        sensor_msgs::msg::Image _color_image;
+        sensor_msgs::msg::Image _depth_image;
+        sensor_msgs::msg::CameraInfo _camera_info;
         bool _segmask_received = false;
         geometry_msgs::msg::Pose _sucpose;
         bool _sucpose_received = false;
--- perception_interfaces/src/perception_manager.cpp
+++ perception_interfaces/src/perception_manager.cpp
@@ -18,12 +18,23 @@ PerceptionManager::PerceptionManager() :
     this->get_parameter("segmask_service_name", segmask_service_name);
     this->get_parameter("sucpose_service_name", sucpose_service_name);
 
+    // Create clients
     _client_callback_group = this->create_callback_group(rclcpp::CallbackGroupType::MutuallyExclusive);
     _sucpose_client = this->create_client<perception_interfaces::srv::Sucpose>(sucpose_service_name,
         rmw_qos_profile_services_default, _client_callback_group);
     _segmask_client = this->create_client<perception_interfaces::srv::Segmask>(segmask_service_name,
         rmw_qos_profile_services_default, _client_callback_group);
+
+    // Create subscriptions
+    _subscription_callback_group = this->create_callback_group(rclcpp::CallbackGroupType::MutuallyExclusive);
+    _color_image_sub = this->create_subscription<sensor_msgs::msg::Image>("/head_camera/rgb/image_raw", 5,
+        std::bind(&PerceptionManager::_color_image_callback, this, std::placeholders::_1), _subscription_callback_group);
+    _depth_image_sub = this->create_subscription<sensor_msgs::msg::Image>("/head_camera/depth/image_rect_raw", 5,
+        std::bind(&PerceptionManager::_depth_image_callback, this, std::placeholders::_1), _subscription_callback_group);
+    _camera_info_sub = this->create_subscription<sensor_msgs::msg::CameraInfo>("/head_camera/camera_info", 5,
+        std::bind(&PerceptionManager::_camera_info_callback, this, std::placeholders::_1), _subscription_callback_group);
     
+    // Create service
     _get_suc_pose_service = this->create_service<orbiter_bt::srv::GetSucPose>("get_suc_pose",
         std::bind(&PerceptionManager::_get_suc_pose, this, std::placeholders::_1, std::placeholders::_2));
 }
@@ -33,15 +44,12 @@ void PerceptionManager::_get_suc_pose(const std::shared_ptr<orbiter_bt::srv::Get
 {
     // Retrieve request information
     std::string item = request->item.data;
-    sensor_msgs::msg::Image color_image = request->color_image;
-    sensor_msgs::msg::Image depth_image = request->depth_image;
-    sensor_msgs::msg::CameraInfo camera_info = request->camera_info;
 
     // Call segmentation mask service
     auto segmask_request = std::make_shared<perception_interfaces::srv::Segmask::Request>();
     segmask_request->object_of_interest.data = item;
-    segmask_request->color_image = color_image;
-    segmask_request->camera_info = camera_info;
+    segmask_request->color_image = _color_image;
+    segmask_request->camera_info = _camera_info;
 
     while (!_segmask_client->wait_for_service(std::chrono::seconds(1))) {
         if (!rclcpp::ok()) {
@@ -53,6 +61,7 @@ void PerceptionManager::_get_suc_pose(const std::shared_ptr<orbiter_bt::srv::Get
 
     auto segmask_result = _segmask_client->async_send_request(segmask_request,
          std::bind(&PerceptionManager::_seg_mask_callback, this, std::placeholders::_1));
+    RCLCPP_INFO(this->get_logger(), "Segmentation mask service called");
     
     while (rclcpp::ok() && !_segmask_received) {
         std::this_thread::sleep_for(std::chrono::milliseconds(100));
@@ -61,10 +70,10 @@ void PerceptionManager::_get_suc_pose(const std::shared_ptr<orbiter_bt::srv::Get
 
     // Call suc pose service
     auto sucpose_request = std::make_shared<perception_interfaces::srv::Sucpose::Request>();
-    sucpose_request->color_image = color_image;
-    sucpose_request->depth_image = depth_image;
+    sucpose_request->color_image = _color_image;
+    sucpose_request->depth_image = _depth_image;
     sucpose_request->segmask = _segmask;
-    sucpose_request->camera_info = camera_info;
+    sucpose_request->camera_info = _camera_info;
 
     while (!_sucpose_client->wait_for_service(std::chrono::seconds(1))) {
         if (!rclcpp::ok()) {
@@ -76,6 +85,7 @@ void PerceptionManager::_get_suc_pose(const std::shared_ptr<orbiter_bt::srv::Get
 
     auto sucpose_result = _sucpose_client->async_send_request(sucpose_request,
          std::bind(&PerceptionManager::_suc_pose_callback, this, std::placeholders::_1));
+    RCLCPP_INFO(this->get_logger(), "Suction pose service called");
 
     while (rclcpp::ok() && !_sucpose_received) {
         std::this_thread::sleep_for(std::chrono::milliseconds(100));
