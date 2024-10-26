35deea0bc0b1436d4cdfb0fb81719095c3d2715d
Jinkai <jinkaiq@andrew.cmu.edu>
2024-10-26 14:27:23 -0400

    adding movehead as well as changing move arm

--- orbiter_bt/src/orbiter_moveArmCuRobo.cpp
+++ orbiter_bt/src/orbiter_moveArmCuRobo.cpp
@@ -5,7 +5,7 @@ MoveArm_CuRobo::MoveArm_CuRobo(const std::string &name,
                  const rclcpp::Node::SharedPtr node)
     : BT::StatefulActionNode(name, config),
     node_(node),
-    client(node->create_client<target_client::srv::TargetPose>("target_pose"))
+    client(node->create_client<orbiter_bt::srv::MoveArm>("target_pose"))
 {
     RCLCPP_INFO(node_->get_logger(), "Move Arm CuRobo Version Creating");
 }
@@ -14,6 +14,7 @@ BT::PortsList MoveArm_CuRobo::providedPorts()
 {
     return {
         BT::InputPort<std::string>("arm_goal"), // arm goal in map frame
+        BT::InputPort<std::string>("traj_type"), // arm name
         };
 }
 
@@ -29,12 +30,15 @@ BT::NodeStatus MoveArm_CuRobo::onStart()
     RCLCPP_INFO(node_->get_logger(), "Pose_Serializing: %s", goal.value().c_str());
     auto pose = bt_string_serialize::stringToPose(goal.value());
     RCLCPP_INFO(node_->get_logger(), "Pose_Sending %f, %f, %f", pose.position.x, pose.position.y, pose.position.z);
-    auto request = std::make_shared<target_client::srv::TargetPose::Request>();
+    auto request = std::make_shared<orbiter_bt::srv::MoveArm::Request>();
     geometry_msgs::msg::PoseStamped pose_stamped;
     pose_stamped.pose = pose;
     pose_stamped.header.frame_id = "base_link";
     RCLCPP_INFO(node_->get_logger(), "Pose_Stamped_Sending %f, %f, %f", pose_stamped.pose.position.x, pose_stamped.pose.position.y, pose_stamped.pose.position.z);
     request->target_pose = pose_stamped;
+    int traj_type = bt_string_serialize::stringToInt(getInput<std::string>("traj_type").value());
+    RCLCPP_INFO(node_->get_logger(), "Traj_Type_Sending %d", traj_type);
+    request->traj_type.data = traj_type;
     auto result = client->async_send_request(request, std::bind(&MoveArm_CuRobo::result_callback, this, std::placeholders::_1));
 
 }
@@ -60,7 +64,7 @@ BT::NodeStatus MoveArm_CuRobo::onRunning()
     return BT::NodeStatus::RUNNING;
 }
 
-void MoveArm_CuRobo::result_callback(rclcpp::Client<target_client::srv::TargetPose>::SharedFuture result)
+void MoveArm_CuRobo::result_callback(rclcpp::Client<orbiter_bt::srv::MoveArm>::SharedFuture result)
 {
     RCLCPP_INFO(node_->get_logger(), "Move Arm result get!");
     auto response = result.get();
